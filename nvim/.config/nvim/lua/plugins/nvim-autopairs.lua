13f00abf87063274e0abd8cc48b7a7b9ae5a0edf
chuckpr <chuck.peperanney@gmail.com>
2021-09-08 16:11:15 -0400

    restore vim double quote rule

--- nvim/.config/nvim/lua/plugins/nvim-autopairs.lua
+++ nvim/.config/nvim/lua/plugins/nvim-autopairs.lua
@@ -1,7 +1,7 @@
 local npairs = require("nvim-autopairs")
 -- local Rule = require('nvim-autopairs.rule')
--- local cond = require('nvim-autopairs.conds')
+local cond = require('nvim-autopairs.conds')
 
 npairs.setup({ignored_next_char = string.gsub([[ [%.%%%]%[%w] ]], '%s+', '')})
 
--- npairs.get_rule('"'):with_pair(cond.not_filetypes({"vim"}))
+npairs.get_rule('"'):with_pair(cond.not_filetypes({"vim"}))
