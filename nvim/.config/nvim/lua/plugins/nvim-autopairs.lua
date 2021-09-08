bd41d8ef7d60c4a2a62e872a8fbe44affdbbd017
chuckpr <chuck.peperanney@gmail.com>
2021-09-08 16:10:17 -0400

    fix ignored_next_char for f-strings

--- nvim/.config/nvim/lua/plugins/nvim-autopairs.lua
+++ nvim/.config/nvim/lua/plugins/nvim-autopairs.lua
@@ -1,7 +1,7 @@
 local npairs = require("nvim-autopairs")
 -- local Rule = require('nvim-autopairs.rule')
-local cond = require('nvim-autopairs.conds')
+-- local cond = require('nvim-autopairs.conds')
 
-npairs.get_rule('"'):with_pair(cond.not_filetypes({"vim"}))
+npairs.setup({ignored_next_char = string.gsub([[ [%.%%%]%[%w] ]], '%s+', '')})
 
-npairs.setup({fast_wrap = {map = '<leader>e', hightlight = 'Search'}})
+-- npairs.get_rule('"'):with_pair(cond.not_filetypes({"vim"}))
