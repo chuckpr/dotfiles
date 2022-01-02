d4c4c749588a6021d134da48bb3b27e464d924aa
chuckpr <chuck.peperanney@gmail.com>
2022-01-02 13:31:30 -0500

    switch space to leader

--- nvim/.config/nvim/lua/lsp/lsp-config.lua
+++ nvim/.config/nvim/lua/lsp/lsp-config.lua
@@ -1,4 +1,4 @@
-local nvim_lsp = require('lspconfig')
+local nvim_lsp = require("lspconfig")
 
 local on_attach = function(client, bufnr)
     local function buf_set_keymap(...)
@@ -22,45 +22,33 @@ local on_attach = function(client, bufnr)
     buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
     buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>',
                    opts)
-    buf_set_keymap('n', '<space>wa',
+    buf_set_keymap('n', '<leader>wa',
                    '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<space>wr',
+    buf_set_keymap('n', '<leader>wr',
                    '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    buf_set_keymap('n', '<space>wl',
+    buf_set_keymap('n', '<leader>wl',
                    '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>',
                    opts)
-    buf_set_keymap('n', '<space>D',
+    buf_set_keymap('n', '<leader>D',
                    '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>',
+    buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    buf_set_keymap('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>',
                    opts)
-    buf_set_keymap('n', '<space>e',
+    buf_set_keymap('n', '<leader>e',
                    '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>',
                    opts)
     buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>',
                    opts)
     buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>',
                    opts)
-    buf_set_keymap('n', '<space>q',
+    buf_set_keymap('n', '<leader>q',
                    '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-    buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>',
-                   opts)
+    buf_set_keymap('n', '<leader>f',
+                   '<cmd>lua vim.lsp.buf.formatting_seq_sync()<CR>', opts)
 
 end
 
--- nvim_lsp.pyright.setup {on_attach = on_attach}
-nvim_lsp.pylsp.setup {
-    cmd = {"pylsp", "-v"},
-    on_attach = on_attach,
-    filetypes = {"python"},
-    settings = {
-        pylsp = {
-            plugins = {
-                pyls_isort = {enabled = false}
-            }
-        }
-    }
-}
+nvim_lsp.jedi_language_server.setup {on_attach = on_attach}
 
 nvim_lsp.vimls.setup {on_attach = on_attach}
 
