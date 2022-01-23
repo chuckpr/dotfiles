4c6bdc3a656551186d9f3be530292483686792cb
chuck.peperanney@gmail.com <chuck.peperanney@gmail.com>
2022-01-23 17:29:17 -0500

    auto pair quotes withing brackets

--- nvim/.config/nvim/lua/plugins/nvim-autopairs.lua
+++ nvim/.config/nvim/lua/plugins/nvim-autopairs.lua
@@ -1,4 +1,5 @@
 local npairs = require("nvim-autopairs")
 
 -- This 👇 allows nested auto-pairing in f-strings
-npairs.setup({ignored_next_char = string.gsub([[ [%.%%%]%[%w] ]], '%s+', '')})
+-- This will not autopair if alphanumeric, dot, percent, or quote follows
+npairs.setup({ignored_next_char = string.gsub([[ [%w%.%%%'%"] ]], "%s+", "")})
