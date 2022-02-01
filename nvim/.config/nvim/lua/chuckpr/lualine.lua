3b4a5c9924c3381ae7fcb9a12536b778dd525f75
Chuck Pepe-Ranney <chuck.peperanney@gmail.com>
2022-02-01 12:16:34 -0500

    add tmux status to lualine

--- nvim/.config/nvim/lua/chuckpr/lualine.lua
+++ nvim/.config/nvim/lua/chuckpr/lualine.lua
@@ -1,3 +1,12 @@
+local function tmux()
+    local tmux_session = os.getenv('TMUX')
+    if tmux_session == nil then
+        return 'not in tmux session'
+    else
+        return 'TMUX active'
+    end
+end
+
 require('lualine').setup {
     options = {
         icons_enabled = true,
@@ -10,7 +19,7 @@ require('lualine').setup {
     sections = {
         lualine_a = {'mode'},
         lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filename'},
+        lualine_c = {'filename', {tmux, color = {fg = 'LightCyan'}}},
         lualine_x = {'encoding', 'fileformat', 'filetype'},
         lualine_y = {'progress'},
         lualine_z = {'location'}
